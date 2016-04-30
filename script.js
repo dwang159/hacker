c9c5dc23663c55a8b5b29cf2ebfef3ab4d5e7aed
Daniel Wang <dwang159@gmail.com>
2016-04-30 14:49:49 -0700

    Added hint and interaction
    Hint pops up after 5 min, more interactive messages, refactoring
    in general.

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 /*
-*(c) Copyright 2011 Simone Masiero. Some Rights Reserved. 
+*(c) Copyright 2011 Simone Masiero. Some Rights Reserved.
 *This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 License
 */
 
+var HINT_TIME = 300000
+var QUESTIONS = ["what|What|why|Why|How|how"]
+var PASSWORDS = ["open sesame|letmein|let me in|12345|pwd|password|passwd"]
+var PLEASE = ["pls|please|Please"]
+var LINUX = ["\\bls\\b|\\bcd\\b|\\bdir\\b|\\brm\\b"]
+var GRANT = ["grant"]
+var rtn= new RegExp("\n", "g"); // newline regex
+var rts= new RegExp("\\s", "g"); // whitespace regex
+var rtt= new RegExp("\\t", "g"); // tab regex
 $(
 	function(){
-        
         $("#console").text(">> ");
 		$( document ).keypress(
-			function ( event ) { 
-				Typer.addText( event ); //Capture the keypress event and call the addText, this is executed on page load
+			function ( event ) {
+				Typer.handlePress( event ); //Capture the keypress event and call the addText, this is executed on page load
 			}
 		);
 		$( document ).keydown(
-			function ( event ) { 
-                // ignore backspace and tab
-                if (event.keyCode == 8) {
+			function ( event ) {
+                // override backspace and tab default behavior
+                if (event.keyCode == 8 || event.keyCode == 9) {
                     event.preventDefault();
-                    Typer.addText(event)
+                    Typer.handlePress(event)
                 }
-                else if (event.keyCode == 9) {
-                    event.preventDefault();
-                    Typer.addText(event)
+                else if (event.keyCode == 27) {
+                    Typer.hidepop();
                 }
-
 			}
 		);
 	}
@@ -38,6 +44,9 @@ var Typer={
 	cursorTimer:null,
     // timer for hack mode check
     hackTimer:null,
+    // timer for hint
+    hintTimer:null,
+    trollTimer:null,
     // keystroke rate counter
     keyRate:null,
     // last key pressed (for ignoring autorepeat)
@@ -50,26 +59,27 @@ var Typer={
 	speed:2, // speed of the Typer
 	file:"", //file, must be setted
     hackerMode:false,
+    first:true,
 	init: function(){// initialize Hacker Typer
         // initialize timer for blinking cursor
-        cursorTimer = setInterval(function(){Typer.updLstChr();}, 500); 
+        cursorTimer = setInterval(function(){Typer.updLstChr();}, 500);
         // check hack mode every second
         hackTimer = setInterval(function(){Typer.checkHack();}, 1000);
         $.get(Typer.file,function(data){// get the text file
             Typer.text=data;// save the textfile in Typer.text
         });
 	},
-	
+
 	content:function(){
 		return $("#console").html();// get console content
 	},
-	
+
 	write:function(str){// append to console content
 		$("#console").append(str);
 		return false;
 	},
-	
-	makeAccess:function(){//create Access Granted popUp      FIXME: popup is on top of the page and doesn't show is the page is scrolled
+
+	makeAccess:function(){//create Access Granted popUp
 		Typer.hidepop(); // hide all popups
 		Typer.accessCount=0; //reset count
 		var ddiv=$("<div id='gran'>").html(""); // create new blank div and id "gran"
@@ -82,20 +92,22 @@ var Typer={
 		$("#console").html("");
 		return false;
 	},
-    // unused
-	makeDenied:function(){//create Access Denied popUp      FIXME: popup is on top of the page and doesn't show is the page is scrolled
+
+
+	makeHint:function(){//create Access Granted popUp
 		Typer.hidepop(); // hide all popups
-		Typer.deniedCount=0; //reset count
-		var ddiv=$("<div id='deni'>").html(""); // create new blank div and id "deni"
-		ddiv.addClass("accessDenied");// add class to the div
-		ddiv.html("<h1>ACCESS DENIED</h1>");// set content of div
-		$(document.body).prepend(ddiv);// prepend div to body
+        clearInterval(Typer.hintTimer);
+		var ddiv=$("<div id='hint'>").html(""); // create new blank div and id "gran"
+		ddiv.addClass("accessGranted"); // add class to the div
+		ddiv.html("<h1>GIT GUD, NOOB</h1><br><a target=\"_blank\" href=http://i.imgur.com/iVHfwLc.gifv>Click here for hint</a>, &lt;Esc&gt; to close"); // set content of div
+		$(document.body).prepend(ddiv); // prepend div to body
 		return false;
 	},
-	
+
+
 	hidepop:function(){// remove all existing popups
-		$("#deni").remove();
 		$("#gran").remove();
+		$("#hint").remove();
 	},
 
     // checks if the user is typing fast enough to enter hack mode
@@ -112,16 +124,58 @@ var Typer={
             // clear hack mode variables for slow typing
             Typer.hackMode = false;
             Typer.consecutive = 0;
-            Typer.addText({keyCode:16});
+            $("#console").html(Typer.buffer.replace(rtn,"<br/>").replace(
+                rtt,"&nbsp;&nbsp;&nbsp;&nbsp;").replace(rts,"&nbsp;"));
         }
         // reset keystroke counter
         Typer.keyRate = 0;
     },
-	
-	addText:function(key){//Main function to add the code
-        var rtn= new RegExp("\n", "g"); // newline regex
-        var rts= new RegExp("\\s", "g"); // whitespace regex
-        var rtt= new RegExp("\\t", "g"); // tab regex
+
+    getMessage:function(){
+        pos = Typer.buffer.lastIndexOf("\n");
+        if (pos == -1) {
+            pos = 0;
+        }
+        cmd = Typer.buffer.slice(pos, Typer.buffer.length)
+        if (cmd.match(QUESTIONS)) {
+            return "THAT'S FOR ME TO KNOW AND YOU TO FIND OUT";
+        }
+        else if (cmd.match(PASSWORDS)) {
+            Typer.done = true;
+            Typer.trollTimer = setInterval(Typer.troll, 2000);
+            return "PASSWORD ACCEPTED. ACCESS GRANTED!";
+        }
+        else if (cmd.match(PLEASE)) {
+            Typer.done = true;
+            Typer.trollTimer = setInterval(Typer.troll, 2000);
+            return "WELL, SINCE YOU ASKED NICELY...";
+        }
+        else if (cmd.match(GRANT)) {
+            Typer.done = true;
+            Typer.trollTimer = setInterval(Typer.troll, 2000);
+            return "OK, SURE!";
+        }
+        else if (cmd.match(LINUX)) {
+            return "YOUR CS SKILLS ARE NO MATCH FOR MY PROGRAMMING";
+        }
+        return "ACCESS DENIED";
+    },
+
+    troll:function() {
+        Typer.done = false;
+        clearInterval(Typer.trollTimer);
+        Typer.addText("\nJK LOL\nACCESS DENIED\n>> ")
+    },
+
+    handlePress:function(key){//Main function to add the code
+
+        // Set up hint timer on first keypress
+        if (Typer.first) {
+            Typer.hintTimer = setInterval(function(){Typer.makeHint();}, HINT_TIME);
+            Typer.first = false;
+        }
+
+        // Update typing rate
         if (key.keyCode != Typer.lastKey)
             Typer.keyRate += 1;
 
@@ -129,51 +183,67 @@ var Typer={
 
 		if(!Typer.done){ // if text is loaded
 			var cont=Typer.content(); // get the console content
-			if(cont.substring(cont.length-1,cont.length)=="|") // if the last char is the blinking cursor
-				$("#console").html($("#console").html().substring(0,cont.length-1)); // remove it before adding the text
+            // if the last char is the blinking cursor
+			if(cont.substring(cont.length-1,cont.length)=="|")
+                // remove it before adding the text
+				$("#console").html($("#console").html().substring(0,cont.length-1));
 
             if (Typer.hackMode) {
                 // add speed to index
                 Typer.index += Typer.speed;
 
                 // parse the text for stripping html enities
-                var text=$("<div/>").text(Typer.text.substring(0,Typer.index)).html();
+                var text=$("<div/>").text(Typer.text.substring(0,
+                        Typer.index)).html();
 
                 // replace newline chars with br, tabs with 4 space and blanks with an html blank
-                $("#console").html(text.replace(rtn,"<br/>").replace(rtt,"&nbsp;&nbsp;&nbsp;&nbsp;").replace(rts,"&nbsp;"));
+                $("#console").html(text.replace(rtn,"<br/>").replace(
+                    rtt,"&nbsp;&nbsp;&nbsp;&nbsp;").replace(rts,"&nbsp;"));
             }
             else {
                 var c = String.fromCharCode(key.keyCode);
                 // special hardcoded cases for some non alphanumeric keys
                 if (key.keyCode == 13) {
-                // print denied message on <enter>
-                    c = "\nACCESS DENIED\n>> ";
-                } else if (key.keyCode == 9) {
+                    // print denied message on <enter>
+                    c = "\n" + Typer.getMessage() + "\n>> ";
+                }
+                else if (key.keyCode == 9) {
                     // input tab on <tab>
                     c = "\t";
-                } else if (key.keyCode == 8) {
+                }
+                else if (key.keyCode == 8) {
                     // delete one char on <backspace>
                     c = ""
                     if (!Typer.buffer.endsWith(">> ")) {
-                        Typer.buffer = Typer.buffer.substring(0, Typer.buffer.length - 1);
+                        Typer.buffer = Typer.buffer.slice(0, -1);
                     }
                 }
-                // add char to buffer and output
-                Typer.buffer = Typer.buffer.concat(c)
-                if ((Typer.buffer.match(/\n/g)||[]).length > 40) {
-                    Typer.buffer = Typer.buffer.substring(Typer.buffer.indexOf("\n") + 1, Typer.buffer.length);
-                    Typer.buffer = Typer.buffer.substring(Typer.buffer.indexOf("\n") + 1, Typer.buffer.length);
-                }
-                var text = Typer.buffer;
-                // replace newline chars with br, tabs with 4 space and blanks with an html blank
-                $("#console").html(text.replace(rtn,"<br/>").replace(rtt,"&nbsp;&nbsp;&nbsp;&nbsp;").replace(rts,"&nbsp;"));
+                Typer.addText(c);
             }
 			window.scrollBy(0,50); // scroll to make sure bottom is always visible
 		}
 	},
-	
+
+    addText:function(c) {
+        // add char to buffer and output
+        Typer.buffer = Typer.buffer.concat(c)
+
+        // handle terminal scrolling (add two newlines)
+        if ((Typer.buffer.match(/\n/g)||[]).length > 40) {
+            Typer.buffer = Typer.buffer.substring(
+                Typer.buffer.indexOf("\n") + 1, Typer.buffer.length);
+            Typer.buffer = Typer.buffer.substring(
+                Typer.buffer.indexOf("\n") + 1, Typer.buffer.length);
+        }
+        var text = Typer.buffer;
+        // replace newline chars with br, tabs with 4 space and
+        // blanks with an html blank
+        $("#console").html(text.replace(rtn,"<br/>").replace(
+            rtt,"&nbsp;&nbsp;&nbsp;&nbsp;").replace(rts,"&nbsp;"));
+    },
+
 	updLstChr:function(){ // blinking cursor
-		var cont=this.content(); // get console 
+		var cont=this.content(); // get console
 		if(cont.substring(cont.length-1,cont.length)=="|") // if last char is the cursor
 			$("#console").html($("#console").html().substring(0,cont.length-1)); // remove it
 		else
